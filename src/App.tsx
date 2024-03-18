1cfc63d3075a6b479c2f427745141c65ea7eb322
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-03-18 15:43:17 -0300

    feat: :art: Style UI by Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
 import "./App.css"
 
+import logoImg from './assets/logo.png'
+
 function App() {
 
   return (
-    <div>
+    <div className='container'>
+      <img 
+        src={logoImg} 
+        alt="Logo" 
+      />
+
+      <h2 className='title'>Categorias</h2>
+      <section className='category-area'>
+        <button className='category-button'>Motivação</button>
+        <button className='category-button'>Bem estar</button>
+      </section>
+
+      <button className='button-frase'>Gerar Frase</button>
+
+      <p className='texto-frase'>Alguma Frase vai aparecer aqui!</p>
 
     </div>
   )
